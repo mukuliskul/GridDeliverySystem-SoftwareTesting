26b61c242fd913c0987aadca502f4540138f6af9
Prabhjot Longia <plongia@myseneca.ca>
2023-06-27 23:26:48 -0400

    Source Code Initial commit

---
+++ SourceCode/project-starter/project-starter/main.c
@@ -0,0 +1,20 @@
+/*
+SFT211- Project 
+Section- NEE
+Group- 3
+*/
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include "mapping.h"
+
+int main(void)
+{
+	struct Map baseMap = populateMap();
+	struct Route blueRoute = getBlueRoute();
+	struct Map routeMap = addRoute(&baseMap, &blueRoute);
+
+	printMap(&routeMap, 1, 1);
+
+	return 0;
+}
